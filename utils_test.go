1fb08ab32419d7e48b168f22c3e6cbd537295085
Michael Woolnough <michael.woolnough@gmail.com>
2024-07-16 06:17:25 +0100

    added Unescape tests

---
+++ utils_test.go
@@ -0,0 +1,66 @@
+package python
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestUnescape(t *testing.T) {
+	for n, test := range [...]struct {
+		Input, Output string
+		Err           error
+	}{
+		{
+			Input:  "\"abc\"",
+			Output: "abc",
+		},
+		{
+			Input:  "\"ab\\\"c\"",
+			Output: "ab\"c",
+		},
+		{
+			Input:  "'ab\\\"c'",
+			Output: "ab\"c",
+		},
+		{
+			Input:  "'ab\\'c'",
+			Output: "ab'c",
+		},
+		{
+			Input: "\"ab\nc\"",
+			Err:   ErrInvalidQuoted,
+		},
+		{
+			Input:  "\"\"\"ab\nc\"\"\"",
+			Output: "ab\nc",
+		},
+		{
+			Input: "'ab\nc'",
+			Err:   ErrInvalidQuoted,
+		},
+		{
+			Input:  "'''ab\nc'''",
+			Output: "ab\nc",
+		},
+		{
+			Input: "\"abc\\\"",
+			Err:   ErrInvalidQuoted,
+		},
+		{
+			Input:  "r\"abc\\\"",
+			Output: "abc\\",
+		},
+		{
+			Input:  "R'abc\\'",
+			Output: "abc\\",
+		},
+	} {
+		output, err := Unescape(test.Input)
+
+		if !errors.Is(test.Err, err) {
+			t.Errorf("test %d: expecting error %q, got %q", n+1, test.Err, err)
+		} else if test.Output != output {
+			t.Errorf("test %d: expecting output %q, got %q", n+1, test.Output, output)
+		}
+	}
+}
