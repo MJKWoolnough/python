2aa7f4769b0ee54380893a452202bd7d6735be00
Michael Woolnough <michael.woolnough@gmail.com>
2024-09-02 20:15:11 +0100

    added missing (empty) function bodies

--- wrappable.go
+++ wrappable.go
@@ -4,30 +4,30 @@ type ConditionalWrappable interface {
 	conditionalWrapabble()
 }
 
-func (ConditionalExpression) conditionalWrapabble()
+func (ConditionalExpression) conditionalWrapabble() {}
 
-func (OrTest) conditionalWrapabble()
+func (OrTest) conditionalWrapabble() {}
 
-func (AndTest) conditionalWrapabble()
+func (AndTest) conditionalWrapabble() {}
 
-func (NotTest) conditionalWrapabble()
+func (NotTest) conditionalWrapabble() {}
 
-func (Comparison) conditionalWrapabble()
+func (Comparison) conditionalWrapabble() {}
 
-func (OrExpression) conditionalWrapabble()
+func (OrExpression) conditionalWrapabble() {}
 
-func (XorExpression) conditionalWrapabble()
+func (XorExpression) conditionalWrapabble() {}
 
-func (AndExpression) conditionalWrapabble()
+func (AndExpression) conditionalWrapabble() {}
 
-func (ShiftExpression) conditionalWrapabble()
+func (ShiftExpression) conditionalWrapabble() {}
 
-func (AddExpression) conditionalWrapabble()
+func (AddExpression) conditionalWrapabble() {}
 
-func (MultiplyExpression) conditionalWrapabble()
+func (MultiplyExpression) conditionalWrapabble() {}
 
-func (UnaryExpression) conditionalWrapabble()
+func (UnaryExpression) conditionalWrapabble() {}
 
-func (PowerExpression) conditionalWrapabble()
+func (PowerExpression) conditionalWrapabble() {}
 
-func (PrimaryExpression) conditionalWrapabble()
+func (PrimaryExpression) conditionalWrapabble() {}
