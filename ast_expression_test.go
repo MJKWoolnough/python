ad5161a4205ae3460105d2198c1d0d4c4df755c9
Michael Woolnough <michael.woolnough@gmail.com>
2024-09-14 18:27:50 +0100

    added TestPowerExpression tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -434,3 +434,128 @@ func TestPrimaryExpression(t *testing.T) {
 		return pe, err
 	})
 }
+
+func TestPowerExpression(t *testing.T) {
+	doTests(t, []sourceFn{
+		{`a`, func(t *test, tk Tokens) { // 1
+			t.Output = PowerExpression{
+				PrimaryExpression: PrimaryExpression{
+					Atom: &Atom{
+						Identifier: &tk[0],
+						Tokens:     tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`await a`, func(t *test, tk Tokens) { // 2
+			t.Output = PowerExpression{
+				AwaitExpression: true,
+				PrimaryExpression: PrimaryExpression{
+					Atom: &Atom{
+						Identifier: &tk[2],
+						Tokens:     tk[2:3],
+					},
+					Tokens: tk[2:3],
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{`a ** b`, func(t *test, tk Tokens) { // 3
+			t.Output = PowerExpression{
+				PrimaryExpression: PrimaryExpression{
+					Atom: &Atom{
+						Identifier: &tk[0],
+						Tokens:     tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				UnaryExpression: &UnaryExpression{
+					PowerExpression: &PowerExpression{
+						PrimaryExpression: PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[4],
+								Tokens:     tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+					Tokens: tk[4:5],
+				},
+				Tokens: tk[:5],
+			}
+		}},
+		{`await a ** b`, func(t *test, tk Tokens) { // 4
+			t.Output = PowerExpression{
+				AwaitExpression: true,
+				PrimaryExpression: PrimaryExpression{
+					Atom: &Atom{
+						Identifier: &tk[2],
+						Tokens:     tk[2:3],
+					},
+					Tokens: tk[2:3],
+				},
+				UnaryExpression: &UnaryExpression{
+					PowerExpression: &PowerExpression{
+						PrimaryExpression: PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[6],
+								Tokens:     tk[6:7],
+							},
+							Tokens: tk[6:7],
+						},
+						Tokens: tk[6:7],
+					},
+					Tokens: tk[6:7],
+				},
+				Tokens: tk[:7],
+			}
+		}},
+		{`a ** b ** c`, func(t *test, tk Tokens) { // 5
+			t.Output = PowerExpression{
+				PrimaryExpression: PrimaryExpression{
+					Atom: &Atom{
+						Identifier: &tk[0],
+						Tokens:     tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				UnaryExpression: &UnaryExpression{
+					PowerExpression: &PowerExpression{
+						PrimaryExpression: PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[4],
+								Tokens:     tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						UnaryExpression: &UnaryExpression{
+							PowerExpression: &PowerExpression{
+								PrimaryExpression: PrimaryExpression{
+									Atom: &Atom{
+										Identifier: &tk[8],
+										Tokens:     tk[8:9],
+									},
+									Tokens: tk[8:9],
+								},
+								Tokens: tk[8:9],
+							},
+							Tokens: tk[8:9],
+						},
+						Tokens: tk[4:9],
+					},
+					Tokens: tk[4:9],
+				},
+				Tokens: tk[:9],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var pe PowerExpression
+
+		err := pe.parse(t.Tokens)
+
+		return pe, err
+	})
+}
