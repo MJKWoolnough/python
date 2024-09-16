331a91219115d60f6e454ec13fa9fdb8f38f6623
Michael Woolnough <michael.woolnough@gmail.com>
2024-09-16 19:22:18 +0100

    added valid XorExpression tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -1830,3 +1830,160 @@ func TestAndExpression(t *testing.T) {
 		return ae, err
 	})
 }
+
+func TestXorExpression(t *testing.T) {
+	doTests(t, []sourceFn{
+		{`a`, func(t *test, tk Tokens) { // 1
+			t.Output = XorExpression{
+				AndExpression: AndExpression{
+					ShiftExpression: ShiftExpression{
+						AddExpression: AddExpression{
+							MultiplyExpression: MultiplyExpression{
+								UnaryExpression: UnaryExpression{
+									PowerExpression: &PowerExpression{
+										PrimaryExpression: PrimaryExpression{
+											Atom: &Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											},
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`a^b`, func(t *test, tk Tokens) { // 2
+			t.Output = XorExpression{
+				AndExpression: AndExpression{
+					ShiftExpression: ShiftExpression{
+						AddExpression: AddExpression{
+							MultiplyExpression: MultiplyExpression{
+								UnaryExpression: UnaryExpression{
+									PowerExpression: &PowerExpression{
+										PrimaryExpression: PrimaryExpression{
+											Atom: &Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											},
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				XorExpression: &XorExpression{
+					AndExpression: AndExpression{
+						ShiftExpression: ShiftExpression{
+							AddExpression: AddExpression{
+								MultiplyExpression: MultiplyExpression{
+									UnaryExpression: UnaryExpression{
+										PowerExpression: &PowerExpression{
+											PrimaryExpression: PrimaryExpression{
+												Atom: &Atom{
+													Identifier: &tk[2],
+													Tokens:     tk[2:3],
+												},
+												Tokens: tk[2:3],
+											},
+											Tokens: tk[2:3],
+										},
+										Tokens: tk[2:3],
+									},
+									Tokens: tk[2:3],
+								},
+								Tokens: tk[2:3],
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+					Tokens: tk[2:3],
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{`a ^ b`, func(t *test, tk Tokens) { // 3
+			t.Output = XorExpression{
+				AndExpression: AndExpression{
+					ShiftExpression: ShiftExpression{
+						AddExpression: AddExpression{
+							MultiplyExpression: MultiplyExpression{
+								UnaryExpression: UnaryExpression{
+									PowerExpression: &PowerExpression{
+										PrimaryExpression: PrimaryExpression{
+											Atom: &Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											},
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				XorExpression: &XorExpression{
+					AndExpression: AndExpression{
+						ShiftExpression: ShiftExpression{
+							AddExpression: AddExpression{
+								MultiplyExpression: MultiplyExpression{
+									UnaryExpression: UnaryExpression{
+										PowerExpression: &PowerExpression{
+											PrimaryExpression: PrimaryExpression{
+												Atom: &Atom{
+													Identifier: &tk[4],
+													Tokens:     tk[4:5],
+												},
+												Tokens: tk[4:5],
+											},
+											Tokens: tk[4:5],
+										},
+										Tokens: tk[4:5],
+									},
+									Tokens: tk[4:5],
+								},
+								Tokens: tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+					Tokens: tk[4:5],
+				},
+				Tokens: tk[:5],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var xe XorExpression
+
+		err := xe.parse(t.Tokens)
+
+		return xe, err
+	})
+}
