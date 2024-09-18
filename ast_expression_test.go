bfc723b7f658a1c193014d2b23e2f1b44dd2308e
Michael Woolnough <michael.woolnough@gmail.com>
2024-09-18 21:48:45 +0100

    added valid AndTest tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -3349,3 +3349,138 @@ func TestNotTest(t *testing.T) {
 		return nt, err
 	})
 }
+
+func TestAndTest(t *testing.T) {
+	doTests(t, []sourceFn{
+		{`a`, func(t *test, tk Tokens) { // 1
+			t.Output = AndTest{
+				NotTest: NotTest{
+					Comparison: Comparison{
+						OrExpression: OrExpression{
+							XorExpression: XorExpression{
+								AndExpression: AndExpression{
+									ShiftExpression: ShiftExpression{
+										AddExpression: AddExpression{
+											MultiplyExpression: MultiplyExpression{
+												UnaryExpression: UnaryExpression{
+													PowerExpression: &PowerExpression{
+														PrimaryExpression: PrimaryExpression{
+															Atom: &Atom{
+																Identifier: &tk[0],
+																Tokens:     tk[:1],
+															},
+															Tokens: tk[:1],
+														},
+														Tokens: tk[:1],
+													},
+													Tokens: tk[:1],
+												},
+												Tokens: tk[:1],
+											},
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`a and b`, func(t *test, tk Tokens) { // 2
+			t.Output = AndTest{
+				NotTest: NotTest{
+					Comparison: Comparison{
+						OrExpression: OrExpression{
+							XorExpression: XorExpression{
+								AndExpression: AndExpression{
+									ShiftExpression: ShiftExpression{
+										AddExpression: AddExpression{
+											MultiplyExpression: MultiplyExpression{
+												UnaryExpression: UnaryExpression{
+													PowerExpression: &PowerExpression{
+														PrimaryExpression: PrimaryExpression{
+															Atom: &Atom{
+																Identifier: &tk[0],
+																Tokens:     tk[:1],
+															},
+															Tokens: tk[:1],
+														},
+														Tokens: tk[:1],
+													},
+													Tokens: tk[:1],
+												},
+												Tokens: tk[:1],
+											},
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				AndTest: &AndTest{
+					NotTest: NotTest{
+						Comparison: Comparison{
+							OrExpression: OrExpression{
+								XorExpression: XorExpression{
+									AndExpression: AndExpression{
+										ShiftExpression: ShiftExpression{
+											AddExpression: AddExpression{
+												MultiplyExpression: MultiplyExpression{
+													UnaryExpression: UnaryExpression{
+														PowerExpression: &PowerExpression{
+															PrimaryExpression: PrimaryExpression{
+																Atom: &Atom{
+																	Identifier: &tk[4],
+																	Tokens:     tk[4:5],
+																},
+																Tokens: tk[4:5],
+															},
+															Tokens: tk[4:5],
+														},
+														Tokens: tk[4:5],
+													},
+													Tokens: tk[4:5],
+												},
+												Tokens: tk[4:5],
+											},
+											Tokens: tk[4:5],
+										},
+										Tokens: tk[4:5],
+									},
+									Tokens: tk[4:5],
+								},
+								Tokens: tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+					Tokens: tk[4:5],
+				},
+				Tokens: tk[:5],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var at AndTest
+
+		err := at.parse(t.Tokens)
+
+		return at, err
+	})
+}
