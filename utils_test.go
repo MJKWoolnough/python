73793ea5b46224598423710a9e06c3a261fff18c
Michael Woolnough <michael.woolnough@gmail.com>
2024-09-23 21:04:41 +0100

    add UnwrapConditional tests

--- utils_test.go
+++ utils_test.go
@@ -883,3 +883,711 @@ func TestWrapConditional(t *testing.T) {
 		}
 	}
 }
+
+func TestUnwrapConditional(t *testing.T) {
+	tks := Tokens{
+		{
+			Token: parser.Token{
+				Type: TokenIdentifier,
+				Data: "a",
+			},
+		},
+		{
+			Token: parser.Token{
+				Type: TokenIdentifier,
+				Data: "b",
+			},
+		},
+	}
+	identA := &tks[0]
+	identB := &tks[1]
+
+	for n, test := range [...]ConditionalWrappable{
+		&Atom{
+			Identifier: identA,
+			Tokens:     tks[:1],
+		},
+		&PrimaryExpression{
+			PrimaryExpression: &PrimaryExpression{
+				Atom: &Atom{
+					Identifier: identA,
+					Tokens:     tks[:1],
+				},
+			},
+			AttributeRef: identB,
+			Tokens:       tks[:2],
+		},
+		&PowerExpression{
+			PrimaryExpression: PrimaryExpression{
+				Atom: &Atom{
+					Identifier: identA,
+					Tokens:     tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			UnaryExpression: &UnaryExpression{
+				PowerExpression: &PowerExpression{
+					PrimaryExpression: PrimaryExpression{
+						Atom: &Atom{
+							Identifier: identB,
+							Tokens:     tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&UnaryExpression{
+			UnaryExpression: &UnaryExpression{
+				PowerExpression: &PowerExpression{
+					PrimaryExpression: PrimaryExpression{
+						Atom: &Atom{
+							Identifier: identB,
+							Tokens:     tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[1:2],
+		},
+		&MultiplyExpression{
+			UnaryExpression: UnaryExpression{
+				PowerExpression: &PowerExpression{
+					PrimaryExpression: PrimaryExpression{
+						Atom: &Atom{
+							Identifier: identA,
+							Tokens:     tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			MultiplyExpression: &MultiplyExpression{
+				UnaryExpression: UnaryExpression{
+					PowerExpression: &PowerExpression{
+						PrimaryExpression: PrimaryExpression{
+							Atom: &Atom{
+								Identifier: identB,
+								Tokens:     tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&AddExpression{
+			MultiplyExpression: MultiplyExpression{
+				UnaryExpression: UnaryExpression{
+					PowerExpression: &PowerExpression{
+						PrimaryExpression: PrimaryExpression{
+							Atom: &Atom{
+								Identifier: identA,
+								Tokens:     tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			AddExpression: &AddExpression{
+				MultiplyExpression: MultiplyExpression{
+					UnaryExpression: UnaryExpression{
+						PowerExpression: &PowerExpression{
+							PrimaryExpression: PrimaryExpression{
+								PrimaryExpression: &PrimaryExpression{
+									Atom: &Atom{
+										Identifier: identB,
+										Tokens:     tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								AttributeRef: identB,
+								Tokens:       tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&ShiftExpression{
+			AddExpression: AddExpression{
+				MultiplyExpression: MultiplyExpression{
+					UnaryExpression: UnaryExpression{
+						PowerExpression: &PowerExpression{
+							PrimaryExpression: PrimaryExpression{
+								Atom: &Atom{
+									Identifier: identA,
+									Tokens:     tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			ShiftExpression: &ShiftExpression{
+				AddExpression: AddExpression{
+					MultiplyExpression: MultiplyExpression{
+						UnaryExpression: UnaryExpression{
+							PowerExpression: &PowerExpression{
+								PrimaryExpression: PrimaryExpression{
+									PrimaryExpression: &PrimaryExpression{
+										Atom: &Atom{
+											Identifier: identB,
+											Tokens:     tks[1:2],
+										},
+										Tokens: tks[1:2],
+									},
+									AttributeRef: identB,
+									Tokens:       tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&AndExpression{
+			ShiftExpression: ShiftExpression{
+				AddExpression: AddExpression{
+					MultiplyExpression: MultiplyExpression{
+						UnaryExpression: UnaryExpression{
+							PowerExpression: &PowerExpression{
+								PrimaryExpression: PrimaryExpression{
+									Atom: &Atom{
+										Identifier: identA,
+										Tokens:     tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			AndExpression: &AndExpression{
+				ShiftExpression: ShiftExpression{
+					AddExpression: AddExpression{
+						MultiplyExpression: MultiplyExpression{
+							UnaryExpression: UnaryExpression{
+								PowerExpression: &PowerExpression{
+									PrimaryExpression: PrimaryExpression{
+										PrimaryExpression: &PrimaryExpression{
+											Atom: &Atom{
+												Identifier: identB,
+												Tokens:     tks[1:2],
+											},
+											Tokens: tks[1:2],
+										},
+										AttributeRef: identB,
+										Tokens:       tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&XorExpression{
+			AndExpression: AndExpression{
+				ShiftExpression: ShiftExpression{
+					AddExpression: AddExpression{
+						MultiplyExpression: MultiplyExpression{
+							UnaryExpression: UnaryExpression{
+								PowerExpression: &PowerExpression{
+									PrimaryExpression: PrimaryExpression{
+										Atom: &Atom{
+											Identifier: identA,
+											Tokens:     tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			XorExpression: &XorExpression{
+				AndExpression: AndExpression{
+					ShiftExpression: ShiftExpression{
+						AddExpression: AddExpression{
+							MultiplyExpression: MultiplyExpression{
+								UnaryExpression: UnaryExpression{
+									PowerExpression: &PowerExpression{
+										PrimaryExpression: PrimaryExpression{
+											PrimaryExpression: &PrimaryExpression{
+												Atom: &Atom{
+													Identifier: identB,
+													Tokens:     tks[1:2],
+												},
+												Tokens: tks[1:2],
+											},
+											AttributeRef: identB,
+											Tokens:       tks[1:2],
+										},
+										Tokens: tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&OrExpression{
+			XorExpression: XorExpression{
+				AndExpression: AndExpression{
+					ShiftExpression: ShiftExpression{
+						AddExpression: AddExpression{
+							MultiplyExpression: MultiplyExpression{
+								UnaryExpression: UnaryExpression{
+									PowerExpression: &PowerExpression{
+										PrimaryExpression: PrimaryExpression{
+											Atom: &Atom{
+												Identifier: identA,
+												Tokens:     tks[:1],
+											},
+											Tokens: tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			OrExpression: &OrExpression{
+				XorExpression: XorExpression{
+					AndExpression: AndExpression{
+						ShiftExpression: ShiftExpression{
+							AddExpression: AddExpression{
+								MultiplyExpression: MultiplyExpression{
+									UnaryExpression: UnaryExpression{
+										PowerExpression: &PowerExpression{
+											PrimaryExpression: PrimaryExpression{
+												PrimaryExpression: &PrimaryExpression{
+													Atom: &Atom{
+														Identifier: identB,
+														Tokens:     tks[1:2],
+													},
+													Tokens: tks[1:2],
+												},
+												AttributeRef: identB,
+												Tokens:       tks[1:2],
+											},
+											Tokens: tks[1:2],
+										},
+										Tokens: tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&Comparison{
+			OrExpression: OrExpression{
+				XorExpression: XorExpression{
+					AndExpression: AndExpression{
+						ShiftExpression: ShiftExpression{
+							AddExpression: AddExpression{
+								MultiplyExpression: MultiplyExpression{
+									UnaryExpression: UnaryExpression{
+										PowerExpression: &PowerExpression{
+											PrimaryExpression: PrimaryExpression{
+												Atom: &Atom{
+													Identifier: identA,
+													Tokens:     tks[:1],
+												},
+												Tokens: tks[:1],
+											},
+											Tokens: tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			Comparisons: []ComparisonExpression{{}},
+		},
+		&NotTest{
+			Nots: 1,
+			Comparison: Comparison{
+				OrExpression: OrExpression{
+					XorExpression: XorExpression{
+						AndExpression: AndExpression{
+							ShiftExpression: ShiftExpression{
+								AddExpression: AddExpression{
+									MultiplyExpression: MultiplyExpression{
+										UnaryExpression: UnaryExpression{
+											PowerExpression: &PowerExpression{
+												PrimaryExpression: PrimaryExpression{
+													Atom: &Atom{
+														Identifier: identA,
+														Tokens:     tks[:1],
+													},
+													Tokens: tks[:1],
+												},
+												Tokens: tks[:1],
+											},
+											Tokens: tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			Tokens: tks[:1],
+		},
+		&AndTest{
+			NotTest: NotTest{
+				Comparison: Comparison{
+					OrExpression: OrExpression{
+						XorExpression: XorExpression{
+							AndExpression: AndExpression{
+								ShiftExpression: ShiftExpression{
+									AddExpression: AddExpression{
+										MultiplyExpression: MultiplyExpression{
+											UnaryExpression: UnaryExpression{
+												PowerExpression: &PowerExpression{
+													PrimaryExpression: PrimaryExpression{
+														Atom: &Atom{
+															Identifier: identA,
+															Tokens:     tks[:1],
+														},
+														Tokens: tks[:1],
+													},
+													Tokens: tks[:1],
+												},
+												Tokens: tks[:1],
+											},
+											Tokens: tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			AndTest: &AndTest{
+				NotTest: NotTest{
+					Comparison: Comparison{
+						OrExpression: OrExpression{
+							XorExpression: XorExpression{
+								AndExpression: AndExpression{
+									ShiftExpression: ShiftExpression{
+										AddExpression: AddExpression{
+											MultiplyExpression: MultiplyExpression{
+												UnaryExpression: UnaryExpression{
+													PowerExpression: &PowerExpression{
+														PrimaryExpression: PrimaryExpression{
+															Atom: &Atom{
+																Identifier: identB,
+																Tokens:     tks[1:2],
+															},
+															Tokens: tks[1:2],
+														},
+														Tokens: tks[1:2],
+													},
+													Tokens: tks[1:2],
+												},
+												Tokens: tks[1:2],
+											},
+											Tokens: tks[1:2],
+										},
+										Tokens: tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&OrTest{
+			AndTest: AndTest{
+				NotTest: NotTest{
+					Comparison: Comparison{
+						OrExpression: OrExpression{
+							XorExpression: XorExpression{
+								AndExpression: AndExpression{
+									ShiftExpression: ShiftExpression{
+										AddExpression: AddExpression{
+											MultiplyExpression: MultiplyExpression{
+												UnaryExpression: UnaryExpression{
+													PowerExpression: &PowerExpression{
+														PrimaryExpression: PrimaryExpression{
+															Atom: &Atom{
+																Identifier: identA,
+																Tokens:     tks[:1],
+															},
+															Tokens: tks[:1],
+														},
+														Tokens: tks[:1],
+													},
+													Tokens: tks[:1],
+												},
+												Tokens: tks[:1],
+											},
+											Tokens: tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			OrTest: &OrTest{
+				AndTest: AndTest{
+					NotTest: NotTest{
+						Comparison: Comparison{
+							OrExpression: OrExpression{
+								XorExpression: XorExpression{
+									AndExpression: AndExpression{
+										ShiftExpression: ShiftExpression{
+											AddExpression: AddExpression{
+												MultiplyExpression: MultiplyExpression{
+													UnaryExpression: UnaryExpression{
+														PowerExpression: &PowerExpression{
+															PrimaryExpression: PrimaryExpression{
+																Atom: &Atom{
+																	Identifier: identB,
+																	Tokens:     tks[1:2],
+																},
+																Tokens: tks[1:2],
+															},
+															Tokens: tks[1:2],
+														},
+														Tokens: tks[1:2],
+													},
+													Tokens: tks[1:2],
+												},
+												Tokens: tks[1:2],
+											},
+											Tokens: tks[1:2],
+										},
+										Tokens: tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+		&ConditionalExpression{
+			OrTest: OrTest{
+				AndTest: AndTest{
+					NotTest: NotTest{
+						Comparison: Comparison{
+							OrExpression: OrExpression{
+								XorExpression: XorExpression{
+									AndExpression: AndExpression{
+										ShiftExpression: ShiftExpression{
+											AddExpression: AddExpression{
+												MultiplyExpression: MultiplyExpression{
+													UnaryExpression: UnaryExpression{
+														PowerExpression: &PowerExpression{
+															PrimaryExpression: PrimaryExpression{
+																Atom: &Atom{
+																	Identifier: identA,
+																	Tokens:     tks[:1],
+																},
+																Tokens: tks[:1],
+															},
+															Tokens: tks[:1],
+														},
+														Tokens: tks[:1],
+													},
+													Tokens: tks[:1],
+												},
+												Tokens: tks[:1],
+											},
+											Tokens: tks[:1],
+										},
+										Tokens: tks[:1],
+									},
+									Tokens: tks[:1],
+								},
+								Tokens: tks[:1],
+							},
+							Tokens: tks[:1],
+						},
+						Tokens: tks[:1],
+					},
+					Tokens: tks[:1],
+				},
+				Tokens: tks[:1],
+			},
+			If: &OrTest{
+				AndTest: AndTest{
+					NotTest: NotTest{
+						Comparison: Comparison{
+							OrExpression: OrExpression{
+								XorExpression: XorExpression{
+									AndExpression: AndExpression{
+										ShiftExpression: ShiftExpression{
+											AddExpression: AddExpression{
+												MultiplyExpression: MultiplyExpression{
+													UnaryExpression: UnaryExpression{
+														PowerExpression: &PowerExpression{
+															PrimaryExpression: PrimaryExpression{
+																Atom: &Atom{
+																	Identifier: identB,
+																	Tokens:     tks[1:2],
+																},
+																Tokens: tks[1:2],
+															},
+															Tokens: tks[1:2],
+														},
+														Tokens: tks[1:2],
+													},
+													Tokens: tks[1:2],
+												},
+												Tokens: tks[1:2],
+											},
+											Tokens: tks[1:2],
+										},
+										Tokens: tks[1:2],
+									},
+									Tokens: tks[1:2],
+								},
+								Tokens: tks[1:2],
+							},
+							Tokens: tks[1:2],
+						},
+						Tokens: tks[1:2],
+					},
+					Tokens: tks[1:2],
+				},
+				Tokens: tks[1:2],
+			},
+			Tokens: tks[:2],
+		},
+	} {
+		if output := UnwrapConditional(WrapConditional(test)); !reflect.DeepEqual(output, test) {
+			t.Errorf("test %d: expecting\n%v\n...got...\n%v", n+1, test, output)
+		}
+	}
+}
