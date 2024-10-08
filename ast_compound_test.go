fa8b596b3ef675fc094949baff3326f99b048d82
Michael Woolnough <michael.woolnough@gmail.com>
2024-10-08 07:33:03 +0100

    add tests for StarredItem

--- ast_compound_test.go
+++ ast_compound_test.go
@@ -2,6 +2,50 @@ package python
 
 import "testing"
 
+func TestStarredItem(t *testing.T) {
+	doTests(t, []sourceFn{
+		{`a`, func(t *test, tk Tokens) { // 1
+			t.Output = StarredItem{
+				AssignmentExpression: &AssignmentExpression{
+					Expression: Expression{
+						ConditionalExpression: WrapConditional(&Atom{
+							Identifier: &tk[0],
+							Tokens:     tk[:1],
+						}),
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`*a`, func(t *test, tk Tokens) { // 2
+			t.Output = StarredItem{
+				OrExpr: &WrapConditional(&Atom{
+					Identifier: &tk[1],
+					Tokens:     tk[1:2],
+				}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+				Tokens: tk[:2],
+			}
+		}},
+		{`* a`, func(t *test, tk Tokens) { // 3
+			t.Output = StarredItem{
+				OrExpr: &WrapConditional(&Atom{
+					Identifier: &tk[2],
+					Tokens:     tk[2:3],
+				}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+				Tokens: tk[:3],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var s StarredItem
+
+		err := s.parse(t.Tokens)
+
+		return s, err
+	})
+}
+
 func TestTypeParam(t *testing.T) {
 	doTests(t, []sourceFn{
 		{`a`, func(t *test, tk Tokens) { // 1
