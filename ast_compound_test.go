a02d6ce388b3ddd2d731528010f7b801d4acb079
Michael Woolnough <michael.woolnough@gmail.com>
2024-10-13 15:30:01 +0100

    add tests for TargetList

--- ast_compound_test.go
+++ ast_compound_test.go
@@ -2,6 +2,156 @@ package python
 
 import "testing"
 
+func TestTargetList(t *testing.T) {
+	doTests(t, []sourceFn{
+		{`a`, func(t *test, tk Tokens) { // 1
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`a,b`, func(t *test, tk Tokens) { // 2
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[2],
+								Tokens:     tk[2:3],
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{`a , b`, func(t *test, tk Tokens) { // 3
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[4],
+								Tokens:     tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+				},
+				Tokens: tk[:5],
+			}
+		}},
+		{`a, ;`, func(t *test, tk Tokens) { // 4
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`a, in`, func(t *test, tk Tokens) { // 5
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`a, =`, func(t *test, tk Tokens) { // 6
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`a,`, func(t *test, tk Tokens) { // 7
+			t.Output = TargetList{
+				Targets: []Target{
+					{
+						PrimaryExpression: &PrimaryExpression{
+							Atom: &Atom{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+				},
+				Tokens: tk[:1],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var tl TargetList
+
+		err := tl.parse(t.Tokens)
+
+		return tl, err
+	})
+}
+
 func TestTarget(t *testing.T) {
 	doTests(t, []sourceFn{
 		{`a`, func(t *test, tk Tokens) { // 1
