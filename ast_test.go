6b3ab7cf4a1b50e0bad04c59e37d1fab87cd16ec
Michael Woolnough <michael.woolnough@gmail.com>
2024-10-27 18:59:27 +0000

    add tests for File

--- ast_test.go
+++ ast_test.go
@@ -184,3 +184,298 @@ func wrapConditionalExpressionError(err Error) Error {
 
 	return err
 }
+
+func TestFile(t *testing.T) {
+	doTests(t, []sourceFn{
+		{`a`, func(t *test, tk Tokens) { // 1
+			t.Output = File{
+				Statements: []Statement{
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{"a\nb", func(t *test, tk Tokens) { // 2
+			t.Output = File{
+				Statements: []Statement{
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[2],
+												Tokens:     tk[2:3],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[2:3],
+										},
+										Tokens: tk[2:3],
+									},
+									Tokens: tk[2:3],
+								},
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{"a\n\nb", func(t *test, tk Tokens) { // 3
+			t.Output = File{
+				Statements: []Statement{
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[2],
+												Tokens:     tk[2:3],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[2:3],
+										},
+										Tokens: tk[2:3],
+									},
+									Tokens: tk[2:3],
+								},
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{"a \nb", func(t *test, tk Tokens) { // 4
+			t.Output = File{
+				Statements: []Statement{
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[3],
+												Tokens:     tk[3:4],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[3:4],
+										},
+										Tokens: tk[3:4],
+									},
+									Tokens: tk[3:4],
+								},
+							},
+							Tokens: tk[3:4],
+						},
+						Tokens: tk[3:4],
+					},
+				},
+				Tokens: tk[:4],
+			}
+		}},
+		{"a\n#A comment\nb", func(t *test, tk Tokens) { // 5
+			t.Output = File{
+				Statements: []Statement{
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[4],
+												Tokens:     tk[4:5],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[4:5],
+										},
+										Tokens: tk[4:5],
+									},
+									Tokens: tk[4:5],
+								},
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+				},
+				Tokens: tk[:5],
+			}
+		}},
+		{"a #A comment\nb", func(t *test, tk Tokens) { // 6
+			t.Output = File{
+				Statements: []Statement{
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[0],
+												Tokens:     tk[:1],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					{
+						StatementList: &StatementList{
+							Statements: []SimpleStatement{
+								{
+									Type: StatementAssignment,
+									AssignmentStatement: &AssignmentStatement{
+										StarredExpression: &StarredExpression{
+											OrExpr: WrapConditional(&Atom{
+												Identifier: &tk[4],
+												Tokens:     tk[4:5],
+											}).OrTest.AndTest.NotTest.Comparison.OrExpression,
+											Tokens: tk[4:5],
+										},
+										Tokens: tk[4:5],
+									},
+									Tokens: tk[4:5],
+								},
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+				},
+				Tokens: tk[:5],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var f File
+
+		err := f.parse(t.Tokens)
+
+		return f, err
+	})
+}
