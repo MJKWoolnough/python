391adb7c5e501b0d3eb353c8c2b8841c75dee863
Michael Woolnough <michael.woolnough@gmail.com>
2025-08-09 19:08:46 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -1987,6 +1987,17 @@ func (f *StarredExpression) printType(w writer, v bool) {
 		pp.WriteString("\nStarredList: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
