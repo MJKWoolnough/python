d4cfaa781a94a0a01635ee6d1a935e10de969935
Michael Woolnough <michael.woolnough@gmail.com>
2025-08-14 07:41:12 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -1532,6 +1532,17 @@ func (f *OrExpression) printType(w writer, v bool) {
 		pp.WriteString("\nOrExpression: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
