ab8566c24851b6429cc68b89036eebaa58d756e4
Michael Woolnough <michael.woolnough@gmail.com>
2025-08-18 07:27:14 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -1770,6 +1770,17 @@ func (f *PowerExpression) printType(w writer, v bool) {
 		pp.WriteString("\nUnaryExpression: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
