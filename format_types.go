b43c08ca083719ecbb5d458ad92bad71c06454f8
Michael Woolnough <michael.woolnough@gmail.com>
2025-08-28 15:25:54 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -1800,6 +1800,17 @@ func (f *PositionalArgument) printType(w writer, v bool) {
 		pp.WriteString("\nExpression: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
