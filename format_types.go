466a68dea479155d2257b84a22a915dc9a8a42e2
Michael Woolnough <michael.woolnough@gmail.com>
2025-09-02 06:47:27 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -2849,6 +2849,17 @@ func (f *WithStatementContents) printType(w writer, v bool) {
 		pp.WriteString("\nItems: []")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
