ffa2d17041fabc3fe6f849343ffbdd668ca6c8ca
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-02 07:36:45 +0100

    added comment print test for ModuleAs

--- format_print_test.go
+++ format_print_test.go
@@ -1580,10 +1580,15 @@ func TestPrintSource(t *testing.T) {
 			"from a import b.c\n",
 			"from a import ( # A\n\tb # B\n\t.c # C\n\n# D\n)\n",
 		},
-		{ // 314
+		{ // 315
 			"from a import ( # A\nb # B\n. # C\nc # D\n\n# E\n)",
 			"from a import b.c\n",
 			"from a import ( # A\n\tb # B\n\t. # C\n\tc # D\n\n# E\n)\n",
+		},
+		{ // 316
+			"from a import ( # A\nb # B\n. # C\nc # D\n\n# E\nas # F\nd # G\n\n# H\n)",
+			"from a import b.c as d\n",
+			"from a import ( # A\n\tb # B\n\t. # C\n\tc # D\n\n\t# E\n\t as # F\n\td # G\n\n# H\n)\n",
 		},
 	} {
 		for m, input := range test {
