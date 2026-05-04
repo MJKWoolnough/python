1941e721b63e8d7a442ac4e47c6efa467ce65a3c
Michael Woolnough <michael.woolnough@gmail.com>
2026-05-04 18:24:25 +0100

    renamed error

--- ast_statement.go
+++ ast_statement.go
@@ -230,7 +230,7 @@ func (s *SimpleStatement) parse(p *pyParser, inReturnable bool) error {
 		p.Score(q)
 	case parser.Token{Type: TokenKeyword, Data: "return"}:
 		if !inReturnable {
-			return p.Error("SimpleStatement", ErrInvalidReturn)
+			return p.Error("SimpleStatement", ErrInvalidStatement)
 		}
 
 		s.ReturnStatement = new(ReturnStatement)
