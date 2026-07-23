22c52a01e6ab8f2887a79a92e21c01c74920ff19
Michael Woolnough <michael.woolnough@gmail.com>
2026-07-23 06:36:09 +0100

    added walk test for Value-only DictItem

--- walk/walk_test.go
+++ walk/walk_test.go
@@ -1285,6 +1285,13 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"File", "Statement", "StatementList", "SimpleStatement", "AssignmentStatement", "StarredExpression", "Expression", "ConditionalExpression", "OrTest", "AndTest", "NotTest", "Comparison", "OrExpression", "XorExpression", "AndExpression", "ShiftExpression", "AddExpression", "MultiplyExpression", "UnaryExpression", "PowerExpression", "PrimaryExpression", "Atom", "Enclosure", "DictDisplay", "ComprehensionFor"},
 		},
+		{ // 180
+			"{**a for b in c}",
+			func(f *python.File) python.Type {
+				return f.Statements[0].StatementList.Statements[0].AssignmentStatement.StarredExpression.Expression.ConditionalExpression.OrTest.AndTest.NotTest.Comparison.OrExpression.XorExpression.AndExpression.ShiftExpression.AddExpression.MultiplyExpression.UnaryExpression.PowerExpression.PrimaryExpression.Atom.Enclosure.DictDisplay.DictItems[0].Value
+			},
+			[]string{"File", "Statement", "StatementList", "SimpleStatement", "AssignmentStatement", "StarredExpression", "Expression", "ConditionalExpression", "OrTest", "AndTest", "NotTest", "Comparison", "OrExpression", "XorExpression", "AndExpression", "ShiftExpression", "AddExpression", "MultiplyExpression", "UnaryExpression", "PowerExpression", "PrimaryExpression", "Atom", "Enclosure", "DictDisplay", "DictItem", "Expression"},
+		},
 		{ // 181
 			"{**a}",
 			func(f *python.File) python.Type {
